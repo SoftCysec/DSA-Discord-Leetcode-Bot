431aab5709301fffadb8aef7f01bf40c77ff56aa
SoftCysec <softengdedan@gmail.com>
2023-08-11 16:53:52 +0300

    Bot configs || Addition of other features

---
+++ README.md
@@ -0,0 +1,42 @@
+# Coding Challenges DSA Bot 🤖
+
+Welcome to the **Coding Challenges DSA Bot** for Discord! This bot is your companion for enhancing your data structure and algorithm skills right within Discord. It fetches coding challenges from LeetCode, giving you and your server members a fresh challenge to tackle every day.
+
+![LeetCode Logo](https://assets.leetcode.com/static_assets/public/webpack_bundles/images/LeetCode_nav.4d940f7b.png)
+
+## 🌟 Features
+
+### 1. Scheduled Challenges 📆
+Every day, without fail, the bot will post challenges in the "code-challenges" channel:
+   - 🌅 **6:00 AM** - An Easy challenge to kickstart your morning
+   - 🌞 **12:00 PM** - A Medium challenge to spice up your afternoon
+   - 🌙 **6:00 PM** - A Hard challenge to test your mettle in the evening
+
+### 2. Personalized Challenge Requests 🎯
+With a simple command, users can request challenges tailored to their preferences:
+   - 🔓 **Free or Paid Challenges**: Specify your preference!
+   - 🌡️ **Difficulty Levels**: Choose between Easy, Medium, or Hard.
+
+The bot will send the challenge to a private channel dedicated to the user, ensuring a personalized experience.
+
+### 3. Private Channels for Members 🚪
+When a new member joins, the bot rolls out the red carpet! It creates a private channel exclusively for that new member, ensuring they have a space for personalized interactions and challenge deliveries.
+
+## 📜 Commands
+
+- `!challenge [free/paid] [easy/medium/hard]`: Fetch a challenge based on your preference.
+- `!scheduled_challenge`: Check the challenge based on the current time schedule.
+
+## 📥 How to Invite the Bot to Your Discord Server
+
+Ready to elevate your server with daily coding challenges? 
+
+Click the link below and authorize the bot for your server:
+
+🔗 [Invite Coding Challenges DSA Bot](https://discord.com/api/oauth2/authorize?client_id=1139507348952600626&permissions=67600&scope=bot)
+
+🚨 **Note**: Ensure you have the necessary permissions to invite bots to your Discord server.
+
+## 📝 License
+
+This bot is open-source and free for use. However, always respect LeetCode's terms of service when using this bot. Happy coding! 🚀
--- api.py
+++ api.py
@@ -1,24 +1,24 @@
-import discord
-from discord.ext import commands
-from dotenv import load_dotenv
 import os
 import requests
 import random
+import discord
+from discord.ext import commands
+from datetime import datetime
+from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-PREFIX = '!problem'
+PREFIX = '!'
 LT_API_URL = 'https://leetcode.com/api/problems/all/'
 PROBLEM_URL_BASE = 'https://leetcode.com/problems/'
-
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
 all_problems = []
 free_problems = []
 paid_problems = []
 
+intents = discord.Intents.all()  # This will enable all intents, including member-related ones
+bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
 class Problem:
     def __init__(self, problem_data):
@@ -29,7 +29,6 @@ class Problem:
         self.paid_only = problem_data['paid_only']
         self.url = PROBLEM_URL_BASE + self.title_slug + '/'
 
-
 def fetch_problems_from_api():
     global all_problems, free_problems, paid_problems
     response = requests.get(LT_API_URL)
@@ -38,42 +37,93 @@ def fetch_problems_from_api():
         for problem_data in data['stat_status_pairs']:
             problem = Problem(problem_data)
             all_problems.append(problem)
-            if problem.paid_only:
-                paid_problems.append(problem)
-            else:
+            if not problem.paid_only:
                 free_problems.append(problem)
+            else:
+                paid_problems.append(problem)
 
+@bot.command(name='challenge')
+async def fetch_challenge(ctx, payment_type: str = 'free', difficulty: str = 'easy'):
+    # Normalize the parameters
+    payment_type = payment_type.lower()
+    difficulty = difficulty.capitalize()
 
-@bot.command(name='random')
-async def random_problem(ctx, type: str = '', difficulty: str = ''):
-    """Get a random problem. Optional arguments: free, paid, easy, medium, hard."""
-    problems = all_problems
-    if type.lower() == 'free':
-        problems = free_problems
-    elif type.lower() == 'paid':
-        problems = paid_problems
+    # Filter problems based on payment type and difficulty
+    problems_to_consider = free_problems if payment_type == 'free' else paid_problems
+    problems_of_difficulty = [problem for problem in problems_to_consider if problem.difficulty == difficulty]
 
-    if difficulty:
-        problems = [problem for problem in problems if problem.difficulty.lower() == difficulty.lower()]
-
-    if not problems:
-        await ctx.send(f"No problems found with given criteria.")
+    if not problems_of_difficulty:
+        await ctx.send(f"No {difficulty} problems available for {payment_type}.")
         return
 
-    selected_problem = random.choice(problems)
-    await ctx.send(f"**{selected_problem.title}**\nDifficulty: {selected_problem.difficulty}\n{selected_problem.url}")
+    selected_problem = random.choice(problems_of_difficulty)
 
+    # Check if the user already has a private channel
+    channel_name = f"{ctx.author.name.lower()}-private"
+    channel = discord.utils.get(ctx.guild.text_channels, name=channel_name)
 
-@bot.command()
-async def info(ctx):
-    """Display information about the number of problems."""
-    await ctx.send(f"LeetCode has {len(all_problems)} problems. {len(free_problems)} are free, {len(paid_problems)} are paid.")
+    # If not, create one
+    if not channel:
+        overwrites = {
+            ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            ctx.author: discord.PermissionOverwrite(read_messages=True)
+        }
+        channel = await ctx.guild.create_text_channel(channel_name, overwrites=overwrites)
 
+    await channel.send(f"**{selected_problem.title}**\nDifficulty: {selected_problem.difficulty}\n{selected_problem.url}")
+
+@bot.command(name='scheduled_challenge')
+async def scheduled_challenge(ctx):
+    current_hour = datetime.now().hour
+
+    # Schedule based on hours
+    schedule = {
+        6: 'Easy',
+        12: 'Medium',
+        18: 'Hard'
+    }
+
+    if current_hour in schedule:
+        difficulty = schedule[current_hour]
+        problems_of_difficulty = [problem for problem in free_problems if problem.difficulty == difficulty]
+        if not problems_of_difficulty:
+            await ctx.send(f"No {difficulty} problems available.")
+            return
+
+        selected_problem = random.choice(problems_of_difficulty)
+
+        # Find the "code-challenges" channel
+        channel = discord.utils.get(ctx.guild.channels, name="code-challenges")
+        if not channel:
+            await ctx.send("Couldn't find the 'code-challenges' channel.")
+            return
+
+        await channel.send(f"**{selected_problem.title}**\nDifficulty: {selected_problem.difficulty}\n{selected_problem.url}")
+    else:
+        await ctx.send("It's not the scheduled time for a challenge.")
+
+@bot.event
+async def on_member_join(member):
+    # Create a private channel for the member
+    channel_name = f"{member.name.lower()}-private"
+    overwrites = {
+        member.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        member: discord.PermissionOverwrite(read_messages=True)
+    }
+    await member.guild.create_text_channel(channel_name, overwrites=overwrites)
 
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
     fetch_problems_from_api()
+    
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandInvokeError):
+        await ctx.send(f"An error occurred while processing your command: {error.original}")
+    else:
+        await ctx.send(f"An error occurred: {error}")
 
 
 bot.run(TOKEN)
